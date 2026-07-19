8a760c9d6ade0f26cdcf5f7febc713b70c2e3e7e
froches2025 <f.michael@alustudent.com>
2026-07-19 17:55:04 +0200

    update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""Function to query hot posts for a given Reddit subreddit."""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts listed for a subreddit."""
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-        return
-
+    """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    params = {"limit": 10}
-
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-
-    if response.status_code != 200:
+    if response.status_code == 404:
         print("None")
         return
-
-    results = response.json().get("data", {})
-    for post in results.get("children", []):
-        print(post.get("data", {}).get("title"))
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
