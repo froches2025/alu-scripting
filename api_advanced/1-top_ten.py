2089b538dd3f6a8785cfc5f28c36c51cbb0a5886
froches2025 <f.michael@alustudent.com>
2026-07-19 17:23:51 +0200

    more bugs

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,9 +5,11 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
     if response.status_code == 200:
         results = response.json().get("data")
